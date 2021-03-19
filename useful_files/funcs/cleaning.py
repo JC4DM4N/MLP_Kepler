75edacae080215cb84b49ec362228e652230c305
Harry Rendell <harryrendell9@gmail.com>
2021-03-19 22:01:47 +0000

    finalising data cleaning and implementing transformer

--- useful_files/funcs/cleaning.py
+++ useful_files/funcs/cleaning.py
@@ -6,64 +6,64 @@ from scipy.interpolate import UnivariateSpline, LSQUnivariateSpline
 from os import listdir
 
 def fit_spline(time,flux,plot=False,nknots=None):
-    """
-    Fits a spline to the time, flux data
+	"""
+	Fits a spline to the time, flux data
 
-    Parameters
-    ----------
-    time : nd_array
-    flux : nd_array
-    plot : boolean
-    	True to plot, False otherwise
-    nkots: int
-    	How tightly to fit the spline
+	Parameters
+	----------
+	time : nd_array
+	flux : nd_array
+	plot : boolean
+		True to plot, False otherwise
+	nkots: int
+		How tightly to fit the spline
 
-    Returns
-    -------
-    tck : #james can you summarise
+	Returns
+	-------
+	tck : #james can you summarise
 
-    """
-    # make sure data x vals are in increasing numbers
-    order = np.argsort(time)
-    flux = flux[order]
-    time = time[order]
+	"""
+	# make sure data x vals are in increasing numbers
+	order = np.argsort(time)
+	flux = flux[order]
+	time = time[order]
 
-    # ensure no nans before fitting
-    mask = ~np.isnan(flux) & ~np.isnan(time)
-    flux = flux[mask]
-    time = time[mask]
+	# ensure no nans before fitting
+	mask = ~np.isnan(flux) & ~np.isnan(time)
+	flux = flux[mask]
+	time = time[mask]
 
-    if not nknots:
-        # knot every 1.5 days...
-        nknots = int((time[-1]-time[0])/0.1)
-        print('default of 1 knot per 0.1 days...')
-    knots = np.linspace(time[0],time[-1],nknots)
+	if not nknots:
+		# knot every 1.5 days...
+		nknots = int((time[-1]-time[0])/0.1)
+		print('default of 1 knot per 0.1 days...')
+	knots = np.linspace(time[0],time[-1],nknots)
 
-    # fit spline...need to check this is correct spline to be fitting
-    tck = LSQUnivariateSpline(time,flux,t=knots[1:-1])
+	# fit spline...need to check this is correct spline to be fitting
+	tck = LSQUnivariateSpline(time,flux,t=knots[1:-1])
 
-    if plot:
-        y = tck(time)
+	if plot:
+		y = tck(time)
 
-        plt.scatter(time,flux,s=0.5)
-        plt.title('Raw data')
-        plt.show()
+		plt.scatter(time,flux,s=0.5)
+		plt.title('Raw data')
+		plt.show()
 
-        plt.plot(time,y)
-        plt.title('Spline fit')
-        plt.show()
-    return tck
+		plt.plot(time,y)
+		plt.title('Spline fit')
+		plt.show()
+	return tck
 
 def cross_validation(time,flux):
 	"""
 	fit spline to half of the data, then minimise RMSE
-		when using the other half of the data.
+			when using the other half of the data.
 
 	bounds: np.array([min, max])
-		upper and lower bounds of knots range
+			upper and lower bounds of knots range
 	returns:
-		nopt: optimal number of knots
-		errmin: RMSE of optimal fit
+			nopt: optimal number of knots
+			errmin: RMSE of optimal fit
 	"""
 
 	def RMSE(y1, y2):
@@ -101,106 +101,138 @@ def cross_validation(time,flux):
 
 
 class clean():
-    def __init__(self, system_id, koi, lc_index=None):
-        """
-        Initialise class clean()
+	def __init__(self, system_id, koi, lc_index=None, slc_or_llc = 'llc'):
+		"""
+		Initialise class clean()
 
-        Parameters
-        ----------
-        system_id : int
-        	kepler system id to be used
-        koi : pandas.DataFrame
-        	DataFrame of the kepler koi meta data
-        lc_index: None or int
-        	if None, load and concatenate all data for the system
-        	if integer n, load the n'th quarter for the system (starts from zero)
+		Parameters
+		----------
+		system_id : int
+				kepler system id to be used
+		koi : pandas.DataFrame
+				DataFrame of the kepler koi meta data
+		lc_index: None or int
+				if None, load and concatenate all data for the system
+				if integer n, load the n'th quarter for the system (starts from zero)
+		slc_or_llc : str
+			either 'slc' or 'llc'. Note that not every system has an slc (but should have llc)
+			Note: slc and llc have average cadences of 1min and 30min respectively.
+		"""
+		self.period = koi.loc[system_id]['koi_period']
+		#finds the date part of the filename
+		fnames = sorted(listdir('../fits/{}/'.format(slc_or_llc)))
+		fnames = [fname[14:31] for fname in fnames if (fname[4:13] == '{:09d}'.format(system_id))]
+		if lc_index == None:
+			t = np.empty(0)
+			f = np.empty(0)
+			e = np.empty(0)
+			for fname in fnames:
+				data = Table.read('../fits/{}/kplr{:09d}-{}.fits'.format(slc_or_llc, system_id, fname))['TIME','PDCSAP_FLUX','PDCSAP_FLUX_ERR']
+				t = np.append(t, data['TIME'])
+				f = np.append(f, data['PDCSAP_FLUX'] - np.nanmean(data['PDCSAP_FLUX']) )#+ 1e4) # subtract the mean
+				e = np.append(e, data['PDCSAP_FLUX_ERR'])
+		else:
+			data = Table.read('../fits/{}/kplr{:09d}-{}.fits'.format(slc_or_llc, system_id, fnames[lc_index]))['TIME','PDCSAP_FLUX','PDCSAP_FLUX_ERR']
+			t = np.array(data['TIME'])
+			f = np.array(data['PDCSAP_FLUX'])
+			e = np.array(data['PDCSAP_FLUX_ERR'])
 
-        """
-        self.period = koi.loc[system_id]['koi_period']
-        #finds the date part of the filename
-        fnames = sorted(listdir('../fits/slc/'))
-        fnames = [fname[14:31] for fname in fnames if (fname[4:13] == '{:09d}'.format(system_id))]
-        if lc_index == None:
-            t = np.empty(0)
-            f = np.empty(0)
-            e = np.empty(0)
-            for fname in fnames:
-                data = Table.read('../fits/slc/kplr{:09d}-{}.fits'.format(system_id, fname))['TIME','PDCSAP_FLUX','PDCSAP_FLUX_ERR']
-                t = np.append(t, data['TIME'])
-                f = np.append(f, data['PDCSAP_FLUX'] - np.nanmean(data['PDCSAP_FLUX']) + 1e4) # subtract the mean
-                e = np.append(e, data['PDCSAP_FLUX_ERR'])
-            else:
-            	data = Table.read('../fits/slc/kplr{:09d}-{}.fits'.format(system_id, fnames[lc_index]))['TIME','PDCSAP_FLUX','PDCSAP_FLUX_ERR']
-            	t = np.array(data['TIME'])
-            	f = np.array(data['PDCSAP_FLUX'])
-            	e = np.array(data['PDCSAP_FLUX_ERR'])
+		self.time = t
+		self.flux = f
+		self.fluxerr = e
+		self.n = len(t)
+		
+		# set to True if data has been cleaned
+		self.clean = False
 
-        self.time = t
-        self.flux = f
-        self.fluxerr = e
-        self.n = len(t)
+	def plot(self):
+		"""
+		Simple scatter plot
+		"""
+		
+		fig, ax = plt.subplots(1,1, figsize=(20,5))
+		ax.scatter(self.time, self.flux, s=0.1)
+		ax.set(ylim = [self.flux.mean()-self.flux.std()*6, self.flux.mean()+self.flux.std()*6])
 
-    def plot(self):
-        """
-        Simple scatter plot
+	def clean_arrays(self, verbose=False):
+		"""
+		Remove nans and sort time, flux, flux error in place
+		"""
+		if verbose:
+			print('no. datapoints prior to cleaning: {}'.format(len(self.time)))
+		# make sure data x vals are in increasing numbers
+		order = np.argsort(self.time)
+		self.flux = self.flux[order]
+		self.time = self.time[order]
+		if isinstance(self.fluxerr,np.ndarray):
+			self.fluxerr = self.fluxerr[order]
 
-        """
-        fig, ax = plt.subplots(1,1, figsize=(20,5))
-        ax.scatter(self.time, self.flux, s=0.1)
-        ax.set(ylim = [self.flux.mean()-self.flux.std()*6, self.flux.mean()+self.flux.std()*6])
+		# ensure no nans before fitting
+		mask = ~np.isnan(self.flux) & ~np.isnan(self.time)
+		self.flux = self.flux[mask]
+		self.time = self.time[mask]
+		if verbose:
+			print('no. datapoints after cleaning: {}'.format(len(self.time)))
+		if isinstance(self.fluxerr,np.ndarray):
+			self.fluxerr = self.fluxerr[mask]
+		
+		self.clean = True
 
-    def clean_arrays(self, verbose=False):
+	def fold(self, plot=True, apply_fold=False, period=None):
+		"""
+		Fold data using the period provided. If none provided, use period from koi
 
-        if verbose:
-            print('no. datapoints prior to cleaning: {}'.format(len(self.time)))
-        # make sure data x vals are in increasing numbers
-        order = np.argsort(self.time)
-        self.flux = self.flux[order]
-        self.time = self.time[order]
-        if isinstance(self.fluxerr,np.ndarray):
-            self.fluxerr = self.fluxerr[order]
+		Parameters
+		----------
+		apply_fold : bool
+			If True, plot and apply the fold to the data. If False, just plot.
+		plot : bool
+			Whether to plot or not
+		"""
+		time = self.time
+		time -= self.time[0]
+		
+		if period is None:
+			period = self.period
+		time = np.mod(time,period)
+		if plot:
+			plt.figure(figsize=(20,5))
+			plt.scatter(time, self.flux,s=0.5)
+			plt.title('folded data on period %s days' %period)
+			plt.ylim([self.flux.mean()-self.flux.std()*12, self.flux.mean()+self.flux.std()*6])
+			plt.show()
+		if apply_fold:
+			self.time = time
 
-        # ensure no nans before fitting
-        mask = ~np.isnan(self.flux) & ~np.isnan(self.time)
-        self.flux = self.flux[mask]
-        self.time = self.time[mask]
-        if verbose:
-            print('no. datapoints after cleaning: {}'.format(len(self.time)))
-        if isinstance(self.fluxerr,np.ndarray):
-            self.fluxerr = self.fluxerr[mask]
+	def remove_stellar_activity(self, verbose=False, plot=False):
+		
+		time = self.time
+		flux = self.flux
+		fluxerr = self.fluxerr
+		
+		# clean arrays if they haven't already
+		if self.clean == False:
+			self.clean_arrays(verbose=1)
 
-    def fold_data(self, plot=True):
-        self.time -= self.time[0]
-        self.time = np.mod(self.time,self.period)
-        if plot:
-            plt.scatter(self.time,self.flux,s=0.5)
-            plt.title('folded data on period %s days' %self.period)
-            plt.figure(figsize=(20,5))
-            plt.show()
+		# get optimal number of knots using cross validation
+		nopt, errmin = cross_validation(time,flux)
+		if verbose:
+			print('optimal number of knots found: %i, err: %.3f' %(nopt, errmin))
+		spl = fit_spline(time, flux, nknots=nopt)
 
-    def remove_stellar_activity(self, obs, verbose=False, plot=False):
+		flux -= spl(time)
+		fluxerr -= spl(time)
 
-        time,flux,fluxerr = clean_arrays(self.time, self.flux, self.fluxerr)
+		if plot:
+			fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
+			ax1.scatter(time,flux+spl(time),s=0.5)
+			ax1.plot(time,spl(time),c='red')
+			ax1.set_title('data with spline overplotted')
 
-        # get optimal number of knots using cross validation
-        nopt, errmin = cross_validation(time,flux)
-        if verbose:
-            print('optimal number of knots found: %i, err: %.3f' %(nopt, errmin))
-        spl = fit_spline(time, flux, nknots=nopt)
+			ax2.scatter(time,flux,s=0.5)
+			ax2.set_title('data with spline removed')
+			plt.show()
 
-        flux -= spl(time)
-        fluxerr -= spl(time)
-
-        if plot:
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
-            ax1.scatter(time,flux+spl(time),s=0.5)
-            ax1.plot(time,spl(time),c='red')
-            ax1.set_title('data with spline overplotted')
-
-            ax2.scatter(time,flux,s=0.5)
-            ax2.set_title('data with spline removed')
-            plt.show()
-
-        
-
-        return time,flux,fluxerr
+		self.time = time
+		self.flux = flux
+		self.fluxerr = fluxerr
